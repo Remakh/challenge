65e3b06703dd097f5549652fbacfea8614c2dd5e
Abhijay Sahay <abhijaygame@gmail.com>
2020-11-13 18:38:16 +0000

    Completed rpn challenges

--- rpnevaluator.py
+++ rpnevaluator.py
@@ -6,5 +6,22 @@ class RpnEvaluator(object):
 
     def evaluate_rpn(self, rpn):
         """Evaluate RPN"""
-        # TODO: add your code here
-        return 85
+
+        tokens = rpn.split(" ")
+        stack = []
+        for token in tokens:
+            try:
+                num = int(token)
+                stack.append(num)
+            except ValueError:
+                operand1 = stack.pop()
+                operand2 = stack.pop()
+                if token == "*":
+                    stack.append(operand1 * operand2)
+                elif token == "+":
+                    stack.append(operand1 + operand2)
+                elif token == "-":
+                    stack.append(operand2 - operand1)
+                else:
+                    stack.append(operand2 // operand1)
+        return stack.pop()
